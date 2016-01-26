35080d7f419ff02dd91c09d4c652efbbd140be82
rahul-islam <rahul.islam3@gmail.com>
2016-01-26 17:33:06 +0530

    add drawLine() to header.

---
+++ cgLib.h
@@ -0,0 +1,11 @@
+#include <graphics.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+#ifndef CGLIB_H_INCLUDED
+#define CGLIB_H_INCLUDED
+
+void drawLine(float x1, float y1, float x2, float y2)
+
+#endif
